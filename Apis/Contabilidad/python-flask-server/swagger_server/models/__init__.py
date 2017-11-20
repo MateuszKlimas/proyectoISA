6a0ef2266021e0866a3ebda5475fb77a2b4c0d9d
2jose2 <jose.yomismo2@gmail.com>
2017-11-20 13:37:45 +0100

    Api contabilidad

---
+++ Apis/Contabilidad/python-flask-server/swagger_server/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+# import models into model package
+from .alumnos import Alumnos
+from .medios_fisicos import MediosFisicos
+from .pas import PAS
+from .profesores import Profesores
