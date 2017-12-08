7403c2a2ffa1150f865114feb672bd345ca6e558
Charlykim <carlos.posa.podean@gmail.com>
2017-12-08 16:09:13 +0100

    Migradas paginas web al servidor flask

--- FlaskWEB/__init__.py
+++ FlaskWEB/__init__.py
@@ -3,12 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return render_template("main.html")
+def interfazMenu():
+    return render_template("interfazMenu.html")
 
-@app.route('/Prueba/')
-def prueba():
-    return render_template("prueba.html")
+@app.route('/interfazAreaPersonal/')
+def interfazAreaPersonal():
+    return render_template("interfazAreaPersonal.html")
+
+@app.route('/interfazProfesores/')
+def interfazProfesores():
+    return render_template("interfazProfesores.html")
+
+@app.route('/interfazUniversidad/')
+def interfazUniversidad():
+    return render_template("interfazUniversidad.html")
+
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=80)
