35b5dc099e8cd489c054ba974cb588fb3191d844
Flavius Stan <flaviusns@outlook.com>
2017-12-08 21:08:36 +0100

    Nuevo Yaml y server.
    Os dejo el yaml de la api y su flask server en zip, para los controller.

--- Apis/python-flask-server Uni/swagger_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-from __future__ import absolute_import
-# import models into model package
-from .asignatura import Asignatura
-from .grado import Grado
-from .universidad import Universidad
