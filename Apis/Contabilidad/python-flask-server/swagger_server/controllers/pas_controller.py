cecd64be210cfc17984d4dbf11828b494ba4c4a8
2jose2 <jose.yomismo2@gmail.com>
2017-12-14 20:24:11 +0100

    Cambio API Contabilidad

--- Apis/Contabilidad/python-flask-server/swagger_server/controllers/pas_controller.py
+++ Apis/Contabilidad/python-flask-server/swagger_server/controllers/pas_controller.py
@@ -6,24 +6,26 @@ from six import iteritems
 from ..util import deserialize_date, deserialize_datetime
 
 
-def add_salario_pas(nominaPas):
+def add_salario_pas(salario_pas):
     """
     Añade un nueva nomina a un pas
     Añades un nuevo salario a un pas.
-    :param nominaPas: El salario que se va a añadir
-    :type nominaPas: int
+    :param salario_pas: Se va añadir un nuevo salario de un pas
+    :type salario_pas: dict | bytes
 
     :rtype: None
     """
+    if connexion.request.is_json:
+        salario_pas = PAS.from_dict(connexion.request.get_json())
     return 'do some magic!'
 
 
-def find_pasby_id(id_profesor):
+def find_pasby_id(id_pas):
     """
     Devuelve el salario de un pas a partir de su codigo de identificacion.
     Devuelve el salario de un pas a partir de su codigo de identificacion.
-    :param id_profesor: id del profesor a buscar su salario
-    :type id_profesor: int
+    :param id_pas: id del pas a buscar su salario
+    :type id_pas: int
 
     :rtype: List[PAS]
     """
