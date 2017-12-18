ba23361af23a29c27070a3db0c1fce25dea97c64
guillecano94 <33253438+guillecano94@users.noreply.github.com>
2017-12-18 16:46:35 +0100

    Update facultades_controller.py

--- Apis/Centro/Centrov2/swagger_server/controllers/facultades_controller.py
+++ Apis/Centro/Centrov2/swagger_server/controllers/facultades_controller.py
@@ -1,4 +1,8 @@
 import connexion
+import psycopg2
+import sys
+import pprint
+import json
 from swagger_server.models.universidad import Universidad
 from datetime import date, datetime
 from typing import List, Dict
@@ -17,7 +21,15 @@ def crear_facultad(facultad):
     """
     if connexion.request.is_json:
         facultad = Universidad.from_dict(connexion.request.get_json())
-    return 'do some magic!'
+
+        
+    conn_string = "host='localhost' dbname='Centros' user='ISA' password='1234'"
+    print ("Connecting to database\n")
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO centro VALUES ("+ repr(facultad.cod_uni)+","+"'"+facultad.name+"'"+","+"'"+(facultad.dir)+"'"+")")
+    conn.commit()
+    return 'Insert realizado '
 
 
 def obtener_facultad(tamanoPagina, numeroPaginas):
@@ -31,7 +43,18 @@ def obtener_facultad(tamanoPagina, numeroPaginas):
 
     :rtype: List[Universidad]
     """
-    return 'do some magic!'
+    conn_string = "host='localhost' dbname='Centros' user='ISA' password='1234'"
+    
+    conn = psycopg2.connect(conn_string)#Nos conectamos 
+ 
+    cursor = conn.cursor()
+ 
+    cursor.execute("SELECT * FROM centro")
+
+    records = json.dumps(cursor.fetchall())
+
+
+    return(records)
 
 
 def remove_facultad(idAsignatura):
@@ -43,7 +66,18 @@ def remove_facultad(idAsignatura):
 
     :rtype: Universidad
     """
-    return 'do some magic!'
+    conn_string = "host='localhost' dbname='Centros' user='ISA' password='1234'"
+    
+    conn = psycopg2.connect(conn_string)#Nos conectamos 
+ 
+    cursor = conn.cursor()
+ 
+    cursor.execute("DELETE FROM centro where centro.id_centro = " + str(idAsignatura))
+    conn.commit()
+
+    
+    return 'Borrado'
+
 
 
 def universidad_get_uni(codUni):
@@ -55,7 +89,17 @@ def universidad_get_uni(codUni):
 
     :rtype: Universidad
     """
-    return 'do some magic!'
+    conn_string = "host='localhost' dbname='Centros' user='ISA' password='1234'"
+    
+    conn = psycopg2.connect(conn_string)#Nos conectamos 
+ 
+    cursor = conn.cursor()
+ 
+    cursor.execute("SELECT \"nombreCentro\" FROM centro WHERE id_centro=" + str(codUni))
+
+
+    records = json.dumps(cursor.fetchall())
+    return records
 
 
 def update_facultad(universidad):
@@ -69,4 +113,13 @@ def update_facultad(universidad):
     """
     if connexion.request.is_json:
         universidad = Universidad.from_dict(connexion.request.get_json())
-    return 'do some magic!'
+
+    conn_string = ("host='localhost' dbname='Centros' user='ISA' password='1234'")
+    print ("Connecting to database\n")
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE public.centro set id_centro =" +str(universidad.cod_uni)+ ",\"nombreCentro\" ="+"'"+universidad.name+"'"+
+                   ",\"direccionCentro\" ="+"'"+ universidad.dir+"'"+"WHERE id_centro= "+str(universidad.cod_uni))
+    conn.commit()
+    return 'Cambio realizdo'
+
