22f5e436f58156ca0888342a79728417e5892b49
OscarDePaz5 <33253695+OscarDePaz5@users.noreply.github.com>
2017-12-18 13:19:43 +0100

    Add files via upload

--- Apis/Departamento Profesores/python-flask-server-departamento-profesores/swagger_server/controllers/docencia_controller.py
+++ Apis/Departamento Profesores/python-flask-server-departamento-profesores/swagger_server/controllers/docencia_controller.py
@@ -1,4 +1,6 @@
 import connexion
+import psycopg2
+import json
 from swagger_server.models.docencia import Docencia
 from datetime import date, datetime
 from typing import List, Dict
@@ -17,18 +19,95 @@ def agregar_docencia(docencia):
     """
     if connexion.request.is_json:
         docencia = Docencia.from_dict(connexion.request.get_json())
-    return 'do some magic!'
+    
+    conn_string = "host='localhost' dbname='DepartamentoProfesores' user='ISA' password='1234'"
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+    doce = docencia
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO \"docencia\" VALUES (" + repr(doce.id_docencia) + "," + repr(doce.id_profesor) + "," + repr(doce.id_gradoAsignatura) + ");")    
+    conn.commit()
+    conn.close()
+    return ""
 
 
-def busqueda_docencia(id_profesor, id_asignaturaGrado):
+def get_docencia_idProf(id_profesor):
     """
     Devuelve relacion de profesor con asignaturas
     
     :param id_profesor: id del profesor
     :type id_profesor: int
+
+    :rtype: Docencia
+    """
+    conn_string = "host='localhost' dbname='DepartamentoProfesores' user='ISA' password='1234'"
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+
+    # execute our Query
+    cursor.execute("SELECT * FROM \"docencia\" WHERE \"docencia\".\"id_profesor\" = " + str(id_profesor) + ";")
+
+    # retrieve the records from the database
+    records = cursor.fetchall()
+    conn.close()
+    return json.dumps(records)
+
+def get_docencia_idAsig(id_asignaturaGrado):
+    """
+    Devuelve relacion de profesor con asignaturas
+
     :param id_asignaturaGrado: id de la asignatura de un grado
     :type id_asignaturaGrado: int
 
     :rtype: Docencia
     """
-    return 'do some magic!'
+
+    conn_string = "host='localhost' dbname='DepartamentoProfesores' user='ISA' password='1234'"
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+
+    # execute our Query
+    cursor.execute("SELECT * FROM \"docencia\" WHERE \"docencia\".\"id_gradoAsignatura\" = " + str(id_asignaturaGrado) + ";")
+
+    # retrieve the records from the database
+    records = cursor.fetchall()
+    conn.close()
+    return json.dumps(records)
+
+def actualizar_docencia(docencia):
+
+    if connexion.request.is_json:
+        docencia = Docencia.from_dict(connexion.request.get_json())
+
+    conn_string = "host='localhost' dbname='DepartamentoProfesores' user='ISA' password='1234'"
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+    doce = docencia
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+    cursor.execute("UPDATE \"docencia\" SET \"docencia\".\"id_docencia\" = " + repr(doce.id_docencia) + ", \"docencia\".\"id_profesor\" = " + repr(doce.id_profesor) ", \"docencia\".\"id_gradoAsignatura\" = " + repr(doce.id_gradoAsignatura) + " WHERE id_docencia = " + repr(doce.id_docencia) + ";")    
+    conn.commit()
+    conn.close()
+    return ""
+
+def eliminar_ingreso(idDocencia):
+
+    conn_string = "host='localhost' dbname='DepartamentoProfesores' user='ISA' password='1234'"
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+
+    # execute our Query
+    cursor.execute("DELETE FROM \"docencia\" where \"docencia\".\"id_docencia\" = " + str(idDocencia) + ";")
+    conn.commit()
+    conn.close()
+    return "" 
