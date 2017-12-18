0069ea4b6ae9c2cc906607d3476ce0878225d531
guillecano94 <33253438+guillecano94@users.noreply.github.com>
2017-12-18 19:24:04 +0100

    pas_controler finalizado

--- Apis/Contabilidad/python-flask-server/swagger_server/controllers/pas_controller.py
+++ Apis/Contabilidad/python-flask-server/swagger_server/controllers/pas_controller.py
@@ -1,4 +1,8 @@
 import connexion
+import psycopg2
+import sys
+import pprint
+import json
 from swagger_server.models.pas import PAS
 from datetime import date, datetime
 from typing import List, Dict
@@ -15,9 +19,21 @@ def add_salario_pas(salario_pas):
 
     :rtype: None
     """
+    fecha="2017-12-12"
     if connexion.request.is_json:
         salario_pas = PAS.from_dict(connexion.request.get_json())
-    return 'do some magic!'
+    conn_string = "host='localhost' dbname='DepartamentoContable' user='ISA' password='1234'"
+    
+    conn = psycopg2.connect(conn_string)#Nos conectamos 
+ 
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO \"nominaPAS\" VALUES (" + repr(salario_pas.id_nominda_pas) +
+                   "," + "'" + fecha + "'" + ","+ repr(salario_pas.importe_nomina_pas)
+                   + ","+ repr(salario_pas.pago_nomina_pas_realizado) + ","+  repr(salario_pas.id_departamento_contable)
+                   + ","+ repr(salario_pas.id_pas) + ")")
+    conn.commit()
+    return 'Todo correcto'
 
 
 def find_pasby_id(id_pas):
@@ -29,4 +45,21 @@ def find_pasby_id(id_pas):
 
     :rtype: List[PAS]
     """
-    return 'do some magic!'
+    conn_string = "host='localhost' dbname='DepartamentoContable' user='ISA' password='1234'"
+    
+    conn = psycopg2.connect(conn_string)#Nos conectamos 
+ 
+    cursor = conn.cursor()
+ 
+    cursor.execute("SELECT \"nominaPAS\".\"importeNominaPAS\" FROM \"nominaPAS\" WHERE id_pas="+str(id_pas))
+ 
+    # retrieve the records from the database
+    records = json.dumps(cursor.fetchall())
+    
+
+    return records
+
+
+
+
+
