ed17e69b690e305aee19c6d7bac99d4d01d29816
Flavius Stan <flaviusns@outlook.com>
2018-01-07 20:49:59 +0100

    Commit Número no me acuerdo
    Borro cosas inútiles

--- Casos de Uso/CasoDeUsoPonerNotas.py
+++ Casos de Uso/CasoDeUsoPonerNotas.py
@@ -5,7 +5,14 @@ import requests
 
 def verDocencia():
     ### Esto mostrará por pantalla las docencias disponibles para el profesor.
-    print("Hola")
+    response = requests.get('http://localhost:8080/DepartamentoProfesores/mostrarDocencia?id_profesor=1')
+
+    print("Las asignaturas asignadas a usted son las siguientes,:")
+    json_data = json.loads(response.text)
+    for i in range(len(json_data)):
+        stri = json_data[i]
+        print("La asignatura con id gradoAsignatura: "+ str(stri['id_gradoAsignatura']))
+    entrarEnAsingnatura(input("Introduzca la asignatura en la que quiere acceder: "))
 
 
 
@@ -14,12 +21,7 @@ def verDocencia():
 
 def entrarEnAsingnatura(id_gradoAsignatura):
     ###Esta función sacará todas las matrículas con ese id de grado_asignatura
-
-
-
-
-    print("Hola")
-
+    
     ####EN ESTE METODO LLAMO A QUE ME DEVUELVA TODAS LAS MATRÍCULAS CON SU NOMBRE
 
 def ponerNota(idMatriculacion):
@@ -31,10 +33,11 @@ def ponerNota(idMatriculacion):
     data = { 
         "idGradoAsignatura": 1, 
         "idMatricula": 2, 
-        "nota": 3
+        "nota": nota
     }
     response = requests.post('http://localhost:8080/secretaria-alumnos/Notas', headers=headers, data=json.dumps(data))
     print(str(response.content))
     ###Hago la request para ponerle la nota.
 
-ponerNota(1)
+##ponerNota(1)
+verDocencia()
