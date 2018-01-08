6f8f0f479503448be8c955025ad561612a719be8
guillecano94 <33253438+guillecano94@users.noreply.github.com>
2018-01-08 18:56:30 +0100

    Get_asignaturas_matriculadas, actualizado

--- Apis/Secretaría de alumnos/ServerAlumnos/swagger_server/controllers/asignaturas_controller.py
+++ Apis/Secretaría de alumnos/ServerAlumnos/swagger_server/controllers/asignaturas_controller.py
@@ -34,56 +34,32 @@ def get_asignaturas_disponibles(id_alumno):
 
 
 def get_asignaturas_matriculadas(id_alumno):
-	"""
-	Asignaturas matriculadas
-	Devuelve una lista con las asignaturas en las que esta matriculado el alumno
-	:param id_alumno: id del alumno
-	:type id_alumno: str
-
-	:rtype: List[Asignatura]
-	"""
-	conn_string = "host='localhost' dbname='SecretariaAlumnos' user='ISA' password='1234'"
-	conexion = psycopg2.connect(conn_string)
-	cursor = conexion.cursor()
-
-	cursor.execute("SELECT \"id_gradoAsignatura\" FROM matriculacion WHERE (id_alumno =" + repr(id_alumno) + ")")
-	cursor_rows = cursor.fetchall()
-
-	
-
-	conexion.commit()
-	conexion.close()
-
-	"""
-	[
-		{
-			"codigo_asignatura": 0,
-			"creditos": "string",
-			"nombre": "string",
-			"tipo": "string"
-		}
-	]
-	"""
-
-	lista_json = []
-
-	
-	for row in cursor_rows:
-
-		ip_puerto = 'localhost:8080'
-		uri = 'http://' + ip_puerto + '/Facultad/asignatura/' + repr(row[0])
-		solicitud = requests.get(uri)
-		json_asignatura = solicitud.json()
-
-		json1 = {
-					"codigo_asignatura": row[0],
-					"creditos": json_asignatura['creditosAsignaturas'],
-					"nombre": json_asignatura['nombreAsignatura'],
-					"nombreGrado": json_asignatura['nombreGrado'],
-					"turnoAsignatura": json_asignatura['turnoAsignatura']
-				}
-
-		lista_json.append(json1)
-
-	return lista_json
+    """
+    Asignaturas matriculadas
+    Devuelve una lista con las asignaturas en las que esta matriculado el alumno
+    :param id_alumno: id del alumno
+    :type id_alumno: str
+    :rtype: List[Asignatura]
+    """
+    conn_string = "host='localhost' dbname='SecretariaAlumnos' user='ISA' password='1234'"
+    conexion = psycopg2.connect(conn_string)
+    cursor = conexion.cursor()
+    cursor.execute("SELECT \"id_gradoAsignatura\" FROM matriculacion WHERE (id_alumno =" + repr(id_alumno) + ")")
+    cursor_rows = cursor.fetchall()
+    lista_json = []
+    for i in range(len(records)):
+        ip_puerto='localhost:5003'
+        direccion= ('http://'+ip_puerto+'/Facultad/asignatura/'+ str(records[i][0]))
+        solicitud = requests.get(direccion)
+        json_solicitud = solicitud.json()
+        json1 = {
+            "Codigo_Asignatura":records[i][0],
+            "Creditos": json_solicitud['creditosAsignaturas'],
+            "NombreAsignatura": json_solicitud['nombreAsignatura'],
+            "NombreGrado": json_solicitud['nombreGrado'],
+            "TurnoAsignatura":json_solicitud['turnoAsignatura']
+            }
+        json_list.append(json1)
+        
+    return(json_list)
 
