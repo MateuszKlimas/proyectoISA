e6f1eb6d8cc054521ccad49ce6e921127be71b05
guillecano94 <33253438+guillecano94@users.noreply.github.com>
2018-01-09 18:19:10 +0100

    Crear alumno actualizado

--- Apis/Secretaría de alumnos/ServerAlumnos/swagger_server/controllers/alumnos_controller.py
+++ Apis/Secretaría de alumnos/ServerAlumnos/swagger_server/controllers/alumnos_controller.py
@@ -1,4 +1,10 @@
+
 import connexion
+import requests
+import psycopg2
+import sys
+import pprint
+import json
 from swagger_server.models.alumno import Alumno
 from datetime import date, datetime
 from typing import List, Dict
@@ -15,6 +21,18 @@ def crear_alumno(alumno):
 
     :rtype: None
     """
+    nulo= 'null'
+    conn_string = "host='localhost' dbname='SecretariaAlumnos' user='ISA' password='1234'"
+    conn = psycopg2.connect(conn_string)#Nos conectamos 
+    cursor = conn.cursor()
+    cursor.execute("SELECT id_alumno FROM public.alumno order by id_alumno desc;")
+    records =cursor.fetchall()
+    id_alumno=int(records[0][0]) + 1
     if connexion.request.is_json:
         alumno = Alumno.from_dict(connexion.request.get_json())
-    return 'do some magic!'
+    
+    cursor.execute("INSERT INTO alumno VALUES (" + str(id_alumno) +","+repr(alumno.dni)+ ","+ repr(alumno.nombre) + ","+  repr(alumno.apellido)
+                   + ","+ repr(alumno.direccion) +","+ repr(alumno.correo)+","+ repr(alumno.telefono)+ ","+str(0)+ ")")
+    conn.commit()
+    texto= "Su id de alumno será --> "+ str(id_alumno)
+    return texto
