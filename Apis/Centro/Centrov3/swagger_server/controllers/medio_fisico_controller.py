d8f69860b3433ea1c84010c4d3576e0d0e6e1967
OscarDePaz5 <33253695+OscarDePaz5@users.noreply.github.com>
2018-01-13 20:35:45 +0100

    Add files via upload

--- Apis/Centro/Centrov3/swagger_server/controllers/medio_fisico_controller.py
+++ Apis/Centro/Centrov3/swagger_server/controllers/medio_fisico_controller.py
@@ -13,25 +13,27 @@ def medio_fisico_id_centro_get(idCentro):
     Devuelve una lista con los medios fisicos
     """
     conn_string = "host='localhost' dbname='Centros' user='ISA' password='1234'"
-        # print the connection string we will use to connect
+    # print the connection string we will use to connect
     print ("Connecting to database\n")
-        # get a connection, if a connect cannot be made an exception will be raised here
+    # get a connection, if a connect cannot be made an exception will be raised here
     conn = psycopg2.connect(conn_string)
         # conn.cursor will return a cursor object, you can use this cursor to perform queries
     cursor = conn.cursor()
-        # execute our Query
-    cursor.execute("SELECT id_medio, \"nombreMedioFisico\", \"tipoMedioFisico\", \"precioMedioFisico\", \"capacidadMedioFisico\", id_centro FROM centro WHERE id_centro = idCentro ORDER BY id_centro DESC;")
+    # execute our Query
+    aux = idCentro
+    query = "SELECT \"id_medio\", \"nombreMedioFisico\", \"tipoMedioFisico\", \"precioMedioFisico\", \"capacidadMedioFisico\", \"id_centro\" FROM \"medioFisico\" WHERE \"medioFisico\".id_centro = " + str(idCentro) + " ORDER BY id_centro DESC;"
+    cursor.execute(query)
     records = cursor.fetchall()
     jsons = []
 
     for row in range(cursor.rowcount):
         json1 = {
-        "idMedio" : records[row][0],
-        "nombreMedio" : records[row][1],
-        "tipoMedio" : records[row][2],
-        "precioMedio" : records[row][3],
-        "capacidad" : records[row][4],
-        "idCentro" : records[row][5],
+        "id_medio" : records[row][0],
+        "nombreMedioFisico" : records[row][1],
+        "tipoMedioFisico" : records[row][2],
+        "precioMedioFisico" : records[row][3],
+        "capacidadMedioFisico" : records[row][4],
+        "id_centro" : records[row][5],
         }
         jsons.append(json1)
     conn.close()
