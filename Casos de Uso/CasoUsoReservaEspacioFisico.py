48e345df04a8ef529924a6e30b2310c08e9a2898
OscarDePaz5 <33253695+OscarDePaz5@users.noreply.github.com>
2018-01-13 14:52:58 +0100

    Add files via upload

---
+++ Casos de Uso/CasoUsoReservaEspacioFisico.py
@@ -0,0 +1,84 @@
+import json
+import connexion
+import psycopg2
+import requests
+import time
+
+
+def verMediosFisicos():
+    ### Esto mostrará por pantalla las facultades existentes en la base de datos.
+    id_facultad = input("Introduzca el ID de la facultad donde quiere hacer la reserva: ")
+
+    response = requests.get('http://localhost:8082/Facultad/MedioFisico/' + id_facultad)
+
+    print("\nLos medios fisicos de esta facultad son los siguientes:")
+    json_data = json.loads(response.text)
+
+    for i in range(len(json_data)):
+        stri = json_data[i]
+        print("ID Medio -> " + str(stri['id_medio']) + " Nombre -> " + str(stri['nombreMedioFisico']) + " Tipo -> " + str(stri['tipoMedioFisico']) + " Precio -> " + str(stri['precioMedioFisico']) + " Capacidad -> " + str(stri['capacidadMedioFisico']))
+    n = input("Introduzca el ID del medio que desea reservar: ")
+
+    print("\nAccediendo...\n")
+    reservaMedioFisico(n)
+
+    print("Exit")
+    print("\n")
+    mostrarMiniMenu()
+
+
+def reservaMedioFisico(idMedio):
+    ###Esta función añadirá una nueva reserva con los datos que se introduzcan.
+
+    if int(idMedio) != 0:
+        nombre = input("Introduzca su nombre como titular de la reserva: ")
+        horaInicio = input("Introduzca la hora de inicio de la reserva (formato hh:mm): ")
+        horaFin = input("Introduzca la hora de fin de la reserva (formato hh:mm): ")
+        fecha = input("Introduzca la fecha de la reserva (formato dd/mm/aaaa): ")
+
+        headers = { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+        data = { 
+            "id_reserva": 1, 
+            "titularReserva": nombre, 
+            "horaInicioReserva": horaInicio,
+            "horaFinReserva": horaFin,
+            "fechaReserva": fecha,
+            "id_medio": idMedio
+        }
+        response = requests.post('http://localhost:8082/Facultad/Reserva', headers = headers, data = json.dumps(data))
+        print(str(response.content))
+    else:
+        print("Error, saliendo del sistema")
+        time.sleep(1)
+        exit()
+    
+
+def mostrarMenu():
+
+    for i in range(5):
+        print("\n")
+    print("\t \t ----------------------------------")
+    print("\t \t Menu de reserva de medios físicos:")
+    print("\t \t ----------------------------------")
+
+    for i in range(3):
+        print("\n")
+    mostrarMiniMenu()
+
+def mostrarMiniMenu():
+    print("Pulse 1 para continuar")
+    print("Pulse 0 para salir del sistema")
+
+
+
+mostrarMenu()
+n = input("Introduzca la opción que desea: ")
+while int(n) != 0:
+    if int(n) == 1:
+        verMediosFisicos()
+        n = input("Introduzca la opción que desea: ")
+print("Saliendo del sistema.")
+time.sleep(1)
