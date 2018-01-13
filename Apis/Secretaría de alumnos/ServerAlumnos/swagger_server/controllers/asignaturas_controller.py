561e5e8718c9af4844423f0b58409b8c86439593
AntonioCortes <anntoniocorteslopez@gmail.com>
2018-01-14 00:59:36 +0100

    he retocado el metodo get_asignaturas_matriculadas

--- Apis/Secretaría de alumnos/ServerAlumnos/swagger_server/controllers/asignaturas_controller.py
+++ Apis/Secretaría de alumnos/ServerAlumnos/swagger_server/controllers/asignaturas_controller.py
@@ -49,7 +49,7 @@ def get_asignaturas_matriculadas(id_alumno):
     lista_json = []
     for i in range(len(records)):
         ip_puerto='localhost:5003'
-        direccion= ('http://'+ip_puerto+'/Facultad/asignatura/'+ str(records[i][0]))
+        direccion= ('http://'+ip_puerto+'/Facultad/Gradoasignatura/'+ str(records[i][0]))
         solicitud = requests.get(direccion)
         json_solicitud = solicitud.json()
         json1 = {
@@ -59,7 +59,7 @@ def get_asignaturas_matriculadas(id_alumno):
             "NombreGrado": json_solicitud['nombreGrado'],
             "TurnoAsignatura":json_solicitud['turnoAsignatura']
             }
-        json_list.append(json1)
+        lista_json.append(json1)
         
-    return(json_list)
+    return lista_json
 
