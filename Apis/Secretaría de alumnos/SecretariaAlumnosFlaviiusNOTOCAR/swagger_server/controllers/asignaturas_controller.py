f8235f5e2e1137c401c4f04921058ccbc08b43cd
Charlykim <carlos.posa.podean@gmail.com>
2018-01-15 14:44:29 +0100

    Añadidos controllers de Mateusz

--- Apis/Secretaría de alumnos/SecretariaAlumnosFlaviiusNOTOCAR/swagger_server/controllers/asignaturas_controller.py
+++ Apis/Secretaría de alumnos/SecretariaAlumnosFlaviiusNOTOCAR/swagger_server/controllers/asignaturas_controller.py
@@ -5,6 +5,9 @@ from datetime import date, datetime
 from typing import List, Dict
 from six import iteritems
 from ..util import deserialize_date, deserialize_datetime
+import psycopg2
+import json
+import requests
 
 
 def consultar_creditos_asignatura(nombre_asignatura):
@@ -20,15 +23,29 @@ def consultar_creditos_asignatura(nombre_asignatura):
 
 
 def get_asignaturas_disponibles(id_alumno):
-    """
-    Asignaturas disponibles para matricular
-    Devuelve una lista con las asignaturas en las que puede matricularse un alumno
-    :param id_alumno: id del alumno
-    :type id_alumno: str
+    conn_string = "host='localhost' dbname='SecretariaAlumnos' user='ISA' password='1234'"
+    asignaturas = []
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()    
+    cursor.execute("SELECT id_grado from alumno where id_alumno=" + id_alumno)
+    records = cursor.fetchall()
+    id_grado = records[0][0]
+    response = requests.get('http://localhost:8082/Facultad/asignaturasGrado?idGrado=' + str(id_grado))
+    json_data = json.loads(response.text)
+    cursor.execute("SELECT \"id_gradoAsignatura\",aprobado from matriculacion where id_alumno=" + str(id_alumno))
+    records = cursor.fetchall()
 
-    :rtype: List[Asignatura]
-    """
-    return 'do some magic!'
+    aprobadas=[]
+    for i in range(len(records)):
+        if records[i][1]:
+            aprobadas.append(records[i][0])
+    print(aprobadas)
+
+    for i in range(len(json_data)):
+        json1 = json_data[i]
+        if json1["idAsignatura"] not in aprobadas:
+            asignaturas.append(json1)
+    return asignaturas
 
 
 def get_asignaturas_matriculadas(id_alumno):
--- Apis/Secretaría de alumnos/SecretariaAlumnosFlaviiusNOTOCAR/swagger_server/controllers/matricula_controller.py
+++ Apis/Secretaría de alumnos/SecretariaAlumnosFlaviiusNOTOCAR/swagger_server/controllers/matricula_controller.py
@@ -79,16 +79,29 @@ def poner_nota(matriculaAsignatura):
 
 
 def realizar_automatricula(id_usuario, asignaturas=None):
+    conn_string = "host='localhost' dbname='SecretariaAlumnos' user='ISA' password='1234'"
     """
     Automatricula
     Permite a un alumno realizar su automatricula
-    :param id_usuario: id del usuario
+    :param id_usuario: nombre del usuario
     :type id_usuario: str
     :param asignaturas: 
     :type asignaturas: list | bytes
 
     :rtype: None
     """
-    if connexion.request.is_json:
-        asignaturas = [AsignaturaMatricula.from_dict(d) for d in connexion.request.get_json()]
-    return 'do some magic!'
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+ 
+	# conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+ 
+	# execute our Query
+    for i in asignaturas:
+        #Hay que eliminar el id de alumno del array de asignaturas
+        cursor.execute("INSERT INTO matriculacion VALUES (" + str(i['id_matriculacion']) + "," + str(i['numero_convocatoria']) + "," + str(i['numero_convocatoria']) + 
+        "," + str(i['nota']) + "," + str(i['aprobado']) + "," + str(id_usuario) + "," + str(i['codigo_asignatura']) + ")")
+    cursor.execute("SELECT * from matriculacion")
+    records = json.dumps(cursor.fetchall())
+    conn.commit()
+    return records
